e5137d747c00631e011f777935e7bf6692d052f7
Tim Malone <tdmalone@gmail.com>
2017-12-16 16:08:31 +1100

    test: Simple tests for log diffs

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,30 @@
+/**
+ * Simple test suite for (the easily testable parts of) sftp-to-papertrail.
+ *
+ * @author Tim Malone <tdmalone@gmail.com>
+ */
 
 'use strict';
+
+// @see https://github.com/abritinthebay/jest-tobetype
+expect.extend( require( 'jest-tobetype' ) );
 
 const theModule = require( '../' );
 
 test( 'Module exports a handler function', () => {
-  expect( typeof theModule.handler ).toBe( 'function' );
+  expect( theModule.handler ).toBeType( 'function' );
+});
+
+test( 'Comparison function returns a string', () => {
+  expect( theModule.compareLogFiles( 'a', 'b' ) ).toBeType( 'string' );
+});
+
+test( 'Comparison function correctly pulls out only new lines', () => {
+
+  const oldLines = 'line1\nline2\nline3\n',
+        newLinesOnly = 'line4\nline5\n',
+        allLines = oldLines + newLinesOnly;
+
+  expect( theModule.compareLogFiles( oldLines, allLines ) ).toBe( newLinesOnly.trim() );
+
 });
